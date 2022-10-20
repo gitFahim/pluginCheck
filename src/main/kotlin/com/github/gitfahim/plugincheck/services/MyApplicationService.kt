b57717ad806bca4bfcfb77ea609428abe0b8d79a
GitHub Action <action@github.com>
2022-10-20 14:53:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/gitfahim/plugincheck/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gitfahim/plugincheck/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gitfahim.plugincheck.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gitfahim.plugincheck.MyBundle
 
 class MyApplicationService {
 
